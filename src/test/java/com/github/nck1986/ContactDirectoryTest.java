999765492d22d16c054b6bfd4e6cb32d1f3a3e17
nck1986 <future614@gmail.com>
2020-05-10 02:28:26 +0300

    add test comments

--- src/test/java/com/github/nck1986/ContactDirectoryTest.java
+++ src/test/java/com/github/nck1986/ContactDirectoryTest.java
@@ -1,18 +1,22 @@
 package com.github.nck1986;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.http.MediaType.TEXT_PLAIN;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.method;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
-import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.beans.factory.annotation.*;
+import org.springframework.boot.test.autoconfigure.web.client.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.annotation.Timed;
 import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.web.client.HttpClientErrorException.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestClientTest
@@ -24,60 +28,108 @@ public class ContactDirectoryTest {
     RestTemplate restTemplate;
 
     private final String BASE_URL = "http://some_domain.com";
-    private final String PATH = "/company/%d/users?name=%s";
+    private final String PATH = "/company/%s/users?name=%s";
 
+    /**
+     * Company ID correct and User name correct
+     */
     @Test
     public void companyIdValidUserValid() {
-        mockRestServiceServer.expect(requestTo("/company/777/users?name=Izergil"))
+        String companyId = "0", someName = "Ezekiel";
+        String url = stringURL(companyId, someName);
+        String body = "+71111111111";
+
+        mockRestServiceServer.expect(requestTo(url))
                 .andExpect(method(GET))
-                .andRespond(withSuccess("+71111111111", TEXT_PLAIN));
-        String request = restTemplate.getForObject("/company/777/users?name=Izergil", String.class);
-        assertEquals("+71111111111", request);
+                .andRespond(withSuccess(body, TEXT_PLAIN));
+        String request = restTemplate.getForObject(url, String.class);
+        assertEquals(body, request);
         mockRestServiceServer.verify();
     }
 
-    @Test public void companyIdNotExistUserValid(){
+    /**
+     * Company ID does not exist
+     */
+    @Test
+    public void companyIdNotExistUserValid() {
+        String companyId = "777", someName = "Ezekiel";
+        String url = stringURL(companyId, someName);
+        String body = "+71111111111";
+
+        mockRestServiceServer.expect(requestTo(url))
+                .andExpect(method(GET))
+                .andRespond(withStatus(NOT_FOUND));
+        assertThrows(NotFound.class, () -> restTemplate.getForObject(url, String.class));
+        mockRestServiceServer.verify();
+    }
+
+    /**
+     * Company ID contain not correct symbols or less 0
+     */
+    @Test
+    public void companyIdNotValidUserValid() {
 
     }
 
-    @Test public void companyIdNotValidUserValid(){
+    /**
+     * Company have no contact data
+     */
+    @Test
+    public void companyIdHaveNoContacts() {
 
     }
 
-    @Test public void companyIdHaveNoContacts(){
+    /**
+     * User name contain not correct symbols
+     */
+    @Test
+    public void companyIdValidUserNotValid() {
 
     }
 
-    @Test public void companyIdValidUserNotValid(){
+    /**
+     * User from other company
+     */
+    @Test
+    public void companyIdValidUserNotFromThisCompany() {
 
     }
 
-    @Test public void companyIdValidUserNotFromThisCompany(){
-
-    }
-
-    @Test public void companyIdValidUserNotExist(){
+    /**
+     * User not exist in this company and can not be find
+     */
+    @Test
+    public void companyIdValidUserNotExist() {
 
     }
 
     /**
      * User valid and exist but have no permissions to read contacts data
      */
-    @Test public void companyIdValidUserHaveNoPermissions(){
+    @Test
+    public void companyIdValidUserHaveNoPermissions() {
 
     }
 
     /**
      * User Name is too long (more then 100 symbols)
      */
-    @Test public void companyIdValidUserNameTooLong(){
+    @Test
+    public void companyIdValidUserNameTooLong() {
 
     }
 
     /**
      * Server response time is above 3000 ms
      */
-    @Test public void serverResponseTooLong(){
+    @Timed(millis = 3000)
+    @Test
+    public void serverResponseTooLong() {
 
+    }
+
+    public String stringURL(String companyId, String someName) {
+        String url = BASE_URL + String.format(PATH, companyId, someName);
+        return url;
     }
 }
