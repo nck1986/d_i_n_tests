ad1c36df20485758914364e604f7058b54d54f93
nck1986 <future614@gmail.com>
2020-05-11 11:46:24 +0300

    code refactor

--- src/main/java/com/github/nck1986/Application.java
+++ src/main/java/com/github/nck1986/Application.java
@@ -1,13 +1,12 @@
 package com.github.nck1986;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.springframework.boot.SpringApplication.run;
+
 @SpringBootApplication
-public class Application
-{
-    public static void main(String[] args)
-    {
-        SpringApplication.run(Application.class, args);
+public class Application {
+    public static void main(String[] arguments) {
+        run(Application.class, arguments);
     }
 }
--- src/test/java/com/github/nck1986/ContactDirectoryTest.java
+++ src/test/java/com/github/nck1986/ContactDirectoryTest.java
@@ -1,5 +1,20 @@
 package com.github.nck1986;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureWebClient;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.test.web.client.RequestMatcher;
+import org.springframework.test.web.client.ResponseActions;
+import org.springframework.web.client.HttpClientErrorException.BadRequest;
+import org.springframework.web.client.HttpClientErrorException.Forbidden;
+import org.springframework.web.client.HttpClientErrorException.NotFound;
+import org.springframework.web.client.RestTemplate;
+
+import static java.lang.String.format;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
@@ -11,184 +26,105 @@ import static org.springframework.test.web.client.match.MockRestRequestMatchers.
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.boot.test.autoconfigure.web.client.*;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.client.support.BasicAuthenticationInterceptor;
-import org.springframework.test.annotation.Timed;
-import org.springframework.test.web.client.MockRestServiceServer;
-import org.springframework.web.client.HttpClientErrorException.*;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.concurrent.TimeUnit;
-
 @RestClientTest
 @AutoConfigureWebClient(registerRestTemplate = true)
 public class ContactDirectoryTest {
-    @Autowired
-    MockRestServiceServer mockRestServiceServer;
-    @Autowired
-    RestTemplate restTemplate;
+	@Autowired
+	MockRestServiceServer mockRestServiceServer;
+	@Autowired
+	RestTemplate restTemplate;
 
-    private final String BASE_URL = "http://some_domain.com";
-    private final String PATH = "/company/%s/users?name=%s";
+	final String URL = "/company/%s/users?name=%s";
+	final String URL_PARAMETRIZED = format(URL,
+	                                       "{companyId}",
+	                                       "{userName}");
 
-    /**
-     * Company ID correct and User name correct
-     * <p>
-     * %companyId% - exist and correct
-     * %someName% - exist and correct
-     * HTTP status code - 200
-     */
-    @Test
-    public void companyIdValidUserValid() {
-        String companyId = "0", someName = "Ezekiel";
-        String url = stringURL(companyId, someName);
-        String body = "+71111111111";
+	@ParameterizedTest(name = "User `{1}` of company `{0}` should have contact `{2}`")
+	@CsvFileSource(resources = "/contacts.csv")
+	void ok(String companyId,
+	        String userName,
+	        String contact) {
 
-        mockRestServiceServer.expect(requestTo(url))
-                .andExpect(method(GET))
-                .andRespond(withSuccess(body, TEXT_PLAIN));
-        String request = restTemplate.getForObject(url, String.class);
-        assertEquals(body, request);
-        mockRestServiceServer.verify();
-    }
+		expect(companyId, userName)
+				.andRespond(withSuccess(contact, TEXT_PLAIN));
 
-    /**
-     * Company ID does not exist
-     * <p>
-     * %companyId% - not exist
-     * %someName% - correct
-     * HTTP status code - 404 Not Found
-     */
-    @Test
-    public void companyIdNotExistUserValid() {
-        String companyId = "777", someName = "Izergil";
-        String url = stringURL(companyId, someName);
+		assertEquals(contact,
+		             actual(companyId, userName),
+		             "Wrong Contact");
+		verify();
+	}
 
-        mockRestServiceServer.expect(requestTo(url))
-                .andExpect(method(GET))
-                .andRespond(withStatus(NOT_FOUND));
-        assertThrows(NotFound.class, () -> restTemplate.getForObject(url, String.class));
-        mockRestServiceServer.verify();
-    }
+	@ParameterizedTest(name = "User `{1}` of/or company `{0}` does not exist")
+	@CsvFileSource(resources ="/nonExistentContacts.csv")
+	void notFound(String companyId,
+	              String userName) {
 
-    /**
-     * Company ID contain not correct symbols or less 0 or empty
-     *
-     * %companyId% - invalid value or empty
-     * %someName% - correct
-     * HTTP status code - 400 Bad Request
-     */
-    @Test
-    public void companyIdNotValidUserValid() {
-        String companyId = "-777", someName = "Izergil";
-        String url = stringURL(companyId, someName);
+		expect(companyId, userName)
+				.andRespond(withStatus(NOT_FOUND));
 
-        mockRestServiceServer.expect(requestTo(url))
-                .andExpect(method(GET))
-                .andRespond(withStatus(BAD_REQUEST));
-        assertThrows(BadRequest.class, () -> restTemplate.getForObject(url, String.class));
-        mockRestServiceServer.verify();
-    }
+		assertThrows(NotFound.class,
+		             () -> actual(companyId, userName),
+		             "Contact not exist");
+		verify();
+	}
 
-    /**
-     * Company have no contact data
-     * <p>
-     * HTTP status code - 204 No Content
-     */
-    @Test
-    public void companyIdHaveNoContacts() {
+	@ParameterizedTest(name = "User `{1}` of/or company `{0}` has/have wrong Name/CompanyID")
+	@CsvFileSource(resources ="/wrongContacts.csv")
+	void badRequest(String companyId,
+	                String userName) {
 
-    }
+		expect(companyId, userName)
+				.andRespond(withStatus(BAD_REQUEST));
 
-    /**
-     * User name contain not correct symbols
-     */
-    @Test
-    public void companyIdValidUserNotValid() {
+		assertThrows(BadRequest.class,
+		             () -> actual(companyId, userName),
+		             "User Name or CompanyID is wrong");
+		verify();
+	}
 
-    }
 
-    /**
-     * User from other company
-     */
-    @Test
-    public void companyIdValidUserNotFromThisCompany() {
+	@ParameterizedTest(name = "User `{1}` have no access to company `{0}` contacts")
+	@CsvFileSource(resources = "/forbiddenContacts.csv")
+	void forbidden(String companyId,
+	               String userName) {
 
-    }
+		expect(companyId, userName)
+				.andExpect(withBasicAuthorization())
+				.andRespond(withStatus(FORBIDDEN));
 
-    /**
-     * User not exist in this company and can not be find
-     */
-    @Test
-    public void companyIdValidUserNotExist() {
+		assertThrows(Forbidden.class,
+		             () -> actual(companyId, userName),
+		             "User have no access");
+		verify();
+	}
 
-    }
+	String actual(String companyId,
+	              String userName) {
+		return restTemplate.getForObject(URL_PARAMETRIZED,
+		                                 String.class,
+		                                 companyId,
+		                                 userName);
+	}
 
-    /**
-     * User valid and exist but have no permissions to read contacts data
-     * <p>
-     * HTTP status code - 403 Forbidden
-     */
-    @Test
-    public void companyIdValidUserHaveNoPermissions() {
-        String companyId = "777", someName = "Izergil";
-        String url = stringURL(companyId, someName);
+	ResponseActions expect(String companyId,
+	                       String userName) {
+		return mockRestServiceServer.expect(requestTo(format(URL,
+		                                                     companyId,
+		                                                     userName)))
+		                            .andExpect(method(GET));
+	}
 
-        mockRestServiceServer.expect(requestTo(url))
-                .andExpect(method(GET))
-                .andExpect(header(AUTHORIZATION,
-                        "Basic " + encodeBasicAuth("user", "password", null)))
-                .andRespond(withStatus(FORBIDDEN));
-        restTemplate.getInterceptors()
-                .add(new BasicAuthenticationInterceptor("user", "password"));
-        assertThrows(Forbidden.class, () -> restTemplate.getForObject(url, String.class));
-        mockRestServiceServer.verify();
-    }
+	private void verify() {
+		mockRestServiceServer.verify();
+	}
 
-    /**
-     * User Name is too long (more then 100 symbols)
-     * <p>
-     * HTTP status code - 414 URI Too Long
-     */
-    @Test
-    public void companyIdValidUserNameTooLong() {
-        String companyId = "777", someName = "Izergil";
-        String url = stringURL(companyId, someName);
+	private RequestMatcher withBasicAuthorization() {
+		restTemplate.getInterceptors()
+		            .add(new BasicAuthenticationInterceptor("user", "password"));
 
-        mockRestServiceServer.expect(requestTo(url))
-                .andExpect(method(GET))
-                .andRespond(withStatus(URI_TOO_LONG));
-
-        mockRestServiceServer.verify();
-    }
-
-    /**
-     * Server response time is above 3000 ms
-     * <p>
-     * HTTP status code - 408 Request Timeout
-     */
-    @Timed(millis = 3000)
-    @Test
-    public void serverResponseTooLong() {
-        int TIMEOUT = (int) TimeUnit.SECONDS.toMillis(3);
-        String companyId = "0", someName = "Ezekiel";
-        String url = stringURL(companyId, someName);
-        String body = "+71111111111";
-
-        mockRestServiceServer.expect(requestTo(url))
-                .andExpect(method(GET))
-                .andRespond(withSuccess(body, TEXT_PLAIN));
-        String request = restTemplate.getForObject(url, String.class);
-
-        assertEquals(body, request);
-        mockRestServiceServer.verify();
-    }
-
-    public String stringURL(String companyId, String someName) {
-        String url = BASE_URL + String.format(PATH, companyId, someName);
-        return url;
-    }
+		return header(AUTHORIZATION,
+		              "Basic " + encodeBasicAuth("user",
+		                                         "password",
+		                                         null));
+	}
 }
